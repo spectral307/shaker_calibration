8ceb4fb4a2de8bc81239fbf1e7a47c9f5e6b203d
Anton <spectral307@gmail.com>
2023-04-17 10:02:27 +0300

    Fix packages in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(name="shaker_calibration",
-      version="1.2.1",
-      packages=["shaker_calibration"],
+      version="1.2.2",
+      packages=["shaker_calibration", "shaker_calibration.ui"],
       install_requires=requirements)
