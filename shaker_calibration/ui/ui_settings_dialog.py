2a3b6845dbde89bd48e9d88f1272cfc1799b8c3e
Anton <spectral307@gmail.com>
2023-11-22 10:48:34 +0300

    Add displacement action to settings dialog

--- shaker_calibration/ui/ui_settings_dialog.py
+++ shaker_calibration/ui/ui_settings_dialog.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file 'settings_dialog.ui'
+# Form implementation generated from reading ui file 'shaker_calibration\ui\settings_dialog.ui'
 #
 # Created by: PyQt6 UI code generator 6.3.0
 #
@@ -12,22 +12,25 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_SettingsDialog(object):
     def setupUi(self, SettingsDialog):
         SettingsDialog.setObjectName("SettingsDialog")
-        SettingsDialog.resize(400, 160)
+        SettingsDialog.resize(400, 180)
         self.buttonBox = QtWidgets.QDialogButtonBox(SettingsDialog)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 110, 341, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(30, 130, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Orientation.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.StandardButton.Cancel|QtWidgets.QDialogButtonBox.StandardButton.Ok)
         self.buttonBox.setCenterButtons(True)
         self.buttonBox.setObjectName("buttonBox")
         self.velocity_transform_checkbox = QtWidgets.QCheckBox(SettingsDialog)
-        self.velocity_transform_checkbox.setGeometry(QtCore.QRect(20, 20, 291, 20))
+        self.velocity_transform_checkbox.setGeometry(QtCore.QRect(20, 20, 321, 20))
         self.velocity_transform_checkbox.setObjectName("velocity_transform_checkbox")
         self.extra_f_label = QtWidgets.QLabel(SettingsDialog)
-        self.extra_f_label.setGeometry(QtCore.QRect(20, 50, 361, 16))
+        self.extra_f_label.setGeometry(QtCore.QRect(20, 70, 361, 16))
         self.extra_f_label.setObjectName("extra_f_label")
         self.extra_f_lineedit = QtWidgets.QLineEdit(SettingsDialog)
-        self.extra_f_lineedit.setGeometry(QtCore.QRect(20, 70, 361, 22))
+        self.extra_f_lineedit.setGeometry(QtCore.QRect(20, 90, 361, 22))
         self.extra_f_lineedit.setObjectName("extra_f_lineedit")
+        self.displacement_transform_checkbox = QtWidgets.QCheckBox(SettingsDialog)
+        self.displacement_transform_checkbox.setGeometry(QtCore.QRect(20, 40, 321, 20))
+        self.displacement_transform_checkbox.setObjectName("displacement_transform_checkbox")
 
         self.retranslateUi(SettingsDialog)
         self.buttonBox.accepted.connect(SettingsDialog.accept) # type: ignore
@@ -39,6 +42,7 @@ class Ui_SettingsDialog(object):
         SettingsDialog.setWindowTitle(_translate("SettingsDialog", "Настройки"))
         self.velocity_transform_checkbox.setText(_translate("SettingsDialog", "Преобразовать в чувствительность по скорости"))
         self.extra_f_label.setText(_translate("SettingsDialog", "Интерполировать на дополнительных частотах (через запятую):"))
+        self.displacement_transform_checkbox.setText(_translate("SettingsDialog", "Преобразовать в чувствительность по перемещению"))
 
 
 if __name__ == "__main__":
